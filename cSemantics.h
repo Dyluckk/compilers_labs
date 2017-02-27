baba24d0eaf419a58265f019c2b7ba028ffd78f4
FlowJo-zacharyw <zacharyw@flowjo.com>
2017-02-27 13:26:06 -0800

    test 1 passing

--- cSemantics.h
+++ cSemantics.h
@@ -1,33 +1,36 @@
 #pragma once
 
 class cSemantics : cVisitor {
-  public:
+public:
 
-    void SemanticError(cAstNode *node, string msg)
+  cSemantics() : m_numErrors(0)
+  {}
+
+  void SemanticError(cAstNode *node, string msg)
+  {
+    m_numErrors++;
+
+    node->SetHasError();
+    std::cout << "ERROR: " << msg << "on line " << node->LineNumber() << "\n";
+  }
+
+  void VisitAllNodes(cAstNode *node) {
+    node->Visit(this);
+  }
+
+  int GetNumErrors() {
+    return m_numErrors;
+  }
+
+  void Visit(cVarExprNode *node)
+  {
+    if (node->GetName()->GetDecl() == nullptr)
     {
-      m_numErrors++;
-
-      node->SetHasError();
-      std::cout << "ERROR: " << msg << "on line " << node->LineNumber() << "\n";
+      SemanticError(node, "Symbol " + node->GetName()->GetName() + " not defined ");
     }
+  }
 
-    void VisitAllNodes(cAstNode *node) {
-      node->Visit(this);
-    }
+protected:
 
-    int GetNumErrors(){
-      return m_numErrors;
-    }
-
-    void Visit(cVarExprNode* node)
-    {
-      if(node->GetName()->GetDecl() == nullptr)
-      {
-        SemanticError(node, "Symbol " + node->GetName()->GetName() + " is not defined");
-      }
-    }
-
-
-  protected:
-    int m_numErrors;
+  int m_numErrors;
 };
